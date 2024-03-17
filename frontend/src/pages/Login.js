0da1e99befa9984d0773529e81d125ab7d21c21c
George Chirikov <george.chirikov@metropolia.fi>
2024-03-17 13:40:54 +0200

    fix login with any values, add more intuitive note delete butyton

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,37 +1,35 @@
-import useField from "../hooks/useField";
-import useLogin from "../hooks/useLogin";
-import { useNavigate } from "react-router-dom";
+import useField from '../hooks/useField';
+import useLogin from '../hooks/useLogin';
+import { useNavigate } from 'react-router-dom';
 
 const Login = ({ setIsAuthenticated }) => {
-  const navigate = useNavigate();
-  const email = useField("email");
-  const password = useField("password");
+	const navigate = useNavigate();
+	const email = useField('email');
+	const password = useField('password');
 
-  const { login, error } = useLogin("/api/users/login");
+	const { login, error } = useLogin('/api/users/login');
 
-  const handleFormSubmit = async (e) => {
-    e.preventDefault();
-    await login({ email: email.value, password: password.value });
-    if (!error) {
-      console.log("success");
-      setIsAuthenticated(true);
-      navigate("/");
-    }
-  };
+	const handleFormSubmit = async (e) => {
+		e.preventDefault();
+		await login({ email: email.value, password: password.value });
+		if (!error) {
+			setIsAuthenticated(Boolean(localStorage.getItem('token')));
+			navigate('/');
+		}
+	};
 
-  return (
-    <div className="login">
-      <h3>Login</h3>
-      <form onSubmit={handleFormSubmit}>
-        <label>Email address:</label>
-        <input {...email} />
-        <label>Password:</label>
-        <input {...password} />
-        <button>Log in</button>
-      </form>
-    </div>
-  );
+	return (
+		<div className="login">
+			<h3>Login</h3>
+			<form onSubmit={handleFormSubmit}>
+				<label>Email address:</label>
+				<input {...email} />
+				<label>Password:</label>
+				<input {...password} />
+				<button>Log in</button>
+			</form>
+		</div>
+	);
 };
-
 
 export default Login;
