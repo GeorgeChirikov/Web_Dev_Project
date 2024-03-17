f5a9f057420bbac7bff63612335f8a3f9307f1b9
George Chirikov <george.chirikov@metropolia.fi>
2024-03-17 21:27:53 +0200

    slight changes to styles in create note button

--- frontend/src/components/sideBar.js
+++ frontend/src/components/sideBar.js
@@ -2,8 +2,10 @@ import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { notes } from '../Data'; // Assuming you have a file called 'notes.js' in the '../data' directory
 import Notes from './notes';
+import { useNavigate } from 'react-router-dom';
 const SideBar = () => {
 	const [note, setNote] = useState(notes);
+	const navigate = useNavigate();
 
 	const addNote = () => {
 		const newNote = {
@@ -19,11 +21,15 @@ const SideBar = () => {
 	return (
 		<div className="sideBar-Section">
 			<div className="sideBar">
-				<Link to="/create-note">Create Note</Link>
-				{/*<Notes/>*/}
-				{/* <button className="byttonAddNote" onClick={addNote}>
-        Add Note
-      </button> */}
+				{/* <Link to="/create-note">Create Note</Link> */}
+				<span
+					className="material-symbols-outlined"
+					onClick={() => {
+						navigate('/create-note');
+					}}
+				>
+					Create Note
+				</span>
 			</div>
 		</div>
 	);
